c4d9ae2808042db8b0a7bcabf7dccd69368bc2aa
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2021-10-23 11:58:36 +0530

    Update descending_sort.py

--- descending_sort.py
+++ descending_sort.py
@@ -6,7 +6,7 @@ for k in range(1,n+1):
     k=k+1
 for i in dic:
     for j in dic:
-        if dic[i]<dic[j]:
+        if dic[i]>dic[j]:
             a=dic[i]
             dic[i]=dic[j]
             dic[j]=a
